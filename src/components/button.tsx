9645ab3891111e093ea8ae280cf2b5474e8f58ee
Chantha makara <chantha.makara@institute.pse.ngo>
2025-04-01 14:07:09 +0700

    create folder stuture

---
+++ src/components/button.tsx
@@ -0,0 +1,11 @@
+
+
+const Button = () => {
+    return (
+        <section>
+            <button className="py-2 px-6 bg-black text-white">Explore</button>
+        </section>
+    )
+}
+
+export default Button;
---
+++ src/pages/Home/components/Herosection.tsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+    return (
+        <section>
+        <div>
+            <h1>This is hero sectin</h1></div></section>
+    )
+}
+
+export default HeroSection;
