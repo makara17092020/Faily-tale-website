3878a9ba7dba88497c2693adfd19c45df4a04288
Chantha makara <chantha.makara@institute.pse.ngo>
2025-04-02 15:45:49 +0700

    change card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Layout from './components/layout'  
+// import Layout from './components/layout'  
+import Home from './pages/Home/home'
 
 function App() {
   return (
     <>
     
       <main>
-        <Layout/>
+        {/* <Layout/> */}
+        <Home/>
       </main>
       
     </>
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,11 +1,27 @@
 // import HeroSection from "./components/Herosection"
 import Card from "../../components/card"
 
-export default function Home() {
+const Home = () => {
     return (
         <main>
+            <>
+
             {/* <HeroSection /> */}
-            <Card/>
+            <h1 className="lg:px-10 md:px-15 sm:px-10 px-20 py-5 font-bold text-5xl text-[#983FFD]">
+                Adventure
+            </h1>
+
+            <div className="flex justify-center flex-wrap gap-5">
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+            
+            </>
+            
         </main>
     )
 }
+
+export default Home
