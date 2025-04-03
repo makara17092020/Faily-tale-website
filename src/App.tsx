b809b9a05317cf1ce9cc005e056bb14663d81e5a
Chantha makara <chantha.makara@institute.pse.ngo>
2025-04-03 08:23:32 +0700

    merge button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
+
 import './App.css'
 // import Layout from './components/layout'  
 import Home from './pages/Home/home'
-=======
-import "./App.css";
-import Home from "./pages/Home/home";
+
 
 function App() {
   return (
     <>
-      <Navbar />
       <main>
         <Home />
       </main>
