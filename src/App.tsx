72f3e306f9a608ceda88d782146876b6b5d566a6
Chantha makara <chantha.makara@institute.pse.ngo>
2025-04-07 13:54:33 +0700

    create story page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
 
-import './App.css'
-<<<<<<< HEAD
 import Home from './pages/Home/home'
 import "./App.css";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import Register from "./pages/Register/register";
-// import Home from "./pages/Home/home";
-=======
-import Story from './pages/Story page/story';
->>>>>>> makara
+
 
 
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
     <Home/>
-      <main>
-        <Header/>
-        <Register/>
-        <Footer/>
-      </main>
-       
-=======
-    <Story/>
->>>>>>> makara
     </>
   );
 }
