3095ab83f3aefd76c77a50466589310b8db2f7ee
Chantha makara <chantha.makara@institute.pse.ngo>
2025-04-21 14:19:50 +0700

    meger code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
 
-<<<<<<< HEAD
-import './App.css'
-import Home from "./pages/Home/home";
-=======
-import Home from './pages/home'
 import "./App.css";
 import Story from './pages/story';
+import Home from './pages/home'
 
 
 
->>>>>>> makara
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <main>
-        <Home />
-      </main>
-=======
     <Home/>
 
     <Story/>
->>>>>>> makara
     </>
   );
 }
